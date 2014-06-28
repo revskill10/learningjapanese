b6cd7408ea69fbec81214104278de6c3115749a6
h_dung <h_dung@brycen.com.vn>
2014-06-28 09:10:34 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learningjapanese::Application.config.secret_key_base = 'a29cd145eb7e04b12c8ebdc547f1eb65b83c35510de70943f5003b1a926e0518555964aff9ff054888b496e20d2f3af31bb539076c43e1df3748c9aa92eec42e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Learningjapanese::Application.config.session_store :cookie_store, key: '_learningjapanese_session'
